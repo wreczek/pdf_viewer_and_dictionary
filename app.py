051a58ff5f9db9f1503eed88e79cd71c3a403502
lunesco <neuromind@vp.pl>
2023-04-13 01:57:03 +0200

    add app.py to git

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+import csv
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "./documents"  # change this to the path of your upload folder
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def index():
+    # Get a list of all the PDF files in the upload folder
+    pdf_files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if f.endswith('.pdf')]
+    # Render the index.html template with the list of PDF files
+    return render_template('index.html', pdf_files=pdf_files)
+
+
+@app.route('/unfamiliar_words')
+def unfamiliar_words():
+    # Read the CSV file with the unfamiliar words and their translations
+    with open('./db/unfamiliar_words.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        word_list = list(reader)
+
+    # Render the unfamiliar_words.html template with the list of unfamiliar words
+    return render_template('unfamiliar_words.html', word_list=word_list)
+
