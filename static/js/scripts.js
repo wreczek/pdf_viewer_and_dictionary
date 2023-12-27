e8922c2c5424bcdbc215c9d18b664253a7ad7a71
lunesco <neuromind@vp.pl>
2023-12-27 02:20:01 +0100

    dictionary shows details again, but remove still WIP

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,5 @@
+// scripts.js
+
 function showWordDetails(word, translation, book, date, difficulty) {
     // Display word details in the modal
     document.getElementById('wordDetails').innerHTML = `
@@ -25,12 +27,8 @@ function acceptChanges() {
 }
 
 function removeWord() {
-    // Implement logic to remove the word (delete the record from the database)
-    // You can use AJAX to send a request to the server
-    // After removing the word, you may want to refresh the page or update the table dynamically
-
-    // Get the word ID or any identifier needed for deletion
-    var wordId = /* Add code to get the word ID */;
+    // Get the word ID from the modal (assuming it's in a data attribute)
+    var wordId = document.getElementById('wordDetailsModal').getAttribute('data-word-id');
 
     // Assuming you have a Flask route for deleting words, modify the URL accordingly
     var deleteUrl = `/delete_word/${wordId}`;
