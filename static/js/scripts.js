7dec823542f8a77825e47d887f746db0a788be8c
lunesco <neuromind@vp.pl>
2023-12-27 18:50:20 +0100

    1. move cookie script to scripts.js 2. in pdf_viewer.html use <script> tag

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -105,3 +105,22 @@ function fetchAndRefreshContent() {
 
 // Set an interval to fetch and refresh content every 5 seconds (adjust as needed)
 //setInterval(fetchAndRefreshContent, 3000);
+
+// scripts.js
+
+// Function to set the last position for the PDF viewer
+function setLastPosition(lastPosition, currentFile) {
+    // Set the last position if available
+    if (lastPosition) {
+        document.getElementById('pdfViewer').contentWindow.scrollTo(0, lastPosition);
+    }
+
+    // Save the scroll position on window unload
+    window.addEventListener('beforeunload', function () {
+        var scrollPosition = document.getElementById('pdfViewer').contentWindow.scrollY;
+        document.cookie = `last_position_${currentFile}=${scrollPosition}`;
+    });
+}
+
+// Use the function to set the last position when the page loads
+setLastPosition("{{ last_position }}", "{{ current_file }}");
