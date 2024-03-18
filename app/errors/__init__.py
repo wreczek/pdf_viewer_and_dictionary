659c46ced705bf57c4639bf8b88056a80f5f5841
lunesco <neuromind@vp.pl>
2024-03-18 01:18:03 +0100

    add new errors blueprints [WIP]

---
+++ app/errors/__init__.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+error_bp = Blueprint('error', __name__, template_folder='templates')
---
+++ app/errors/routes.py
@@ -0,0 +1,15 @@
+from flask import render_template
+
+from app.errors import error_bp
+from app.extensions import db
+
+
+@error_bp.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@error_bp.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()  # Rollback the session in case of database errors
+    return render_template('500.html'), 500
