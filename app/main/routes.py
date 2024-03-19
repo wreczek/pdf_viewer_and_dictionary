597828c321f78eda29a9a72757edfce1341e9153
lunesco <neuromind@vp.pl>
2024-03-19 22:11:33 +0100

    add active_page to index endpoint

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,10 +7,12 @@ from app.main import main_bp
 @main_bp.route("/")
 @main_bp.route("/home")
 def index():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', active_page='index')
 
 
 @main_bp.route("/dashboard")
 @login_required
 def dashboard():
-    return render_template('dashboard.html', title='Dashboard')
+    return render_template('dashboard.html',
+                           title='Dashboard',
+                           active_page='dashboard')
